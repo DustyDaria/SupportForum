3f9bf4f510d98f384c99f6b990031333c22b2143
Игнатьева Дарья <IgnatevaDE@osk.org.rzd>
2024-03-25 21:38:58 +0500

    add user template

---
+++ SupportForum/Controllers/UserController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SupportForum.Models;
+
+namespace SupportForum.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+              return _context.TblUsers != null ? 
+                          View(await _context.TblUsers.ToListAsync()) :
+                          Problem("Entity set 'DataContext.TblUsers'  is null.");
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(decimal? id)
+        {
+            if (id == null || _context.TblUsers == null)
+            {
+                return NotFound();
+            }
+
+            var tblUser = await _context.TblUsers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tblUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblUser);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserLogin,FullName,CurrRole,DateLastActivity")] TblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tblUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblUser);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(decimal? id)
+        {
+            if (id == null || _context.TblUsers == null)
+            {
+                return NotFound();
+            }
+
+            var tblUser = await _context.TblUsers.FindAsync(id);
+            if (tblUser == null)
+            {
+                return NotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(decimal id, [Bind("Id,UserLogin,FullName,CurrRole,DateLastActivity")] TblUser tblUser)
+        {
+            if (id != tblUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TblUserExists(tblUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblUser);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(decimal? id)
+        {
+            if (id == null || _context.TblUsers == null)
+            {
+                return NotFound();
+            }
+
+            var tblUser = await _context.TblUsers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tblUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblUser);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(decimal id)
+        {
+            if (_context.TblUsers == null)
+            {
+                return Problem("Entity set 'DataContext.TblUsers'  is null.");
+            }
+            var tblUser = await _context.TblUsers.FindAsync(id);
+            if (tblUser != null)
+            {
+                _context.TblUsers.Remove(tblUser);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TblUserExists(decimal id)
+        {
+          return (_context.TblUsers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
