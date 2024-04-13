56a1795cdfa9d50462a398278e3d87cb64ffb09f
Игнатьева Дарья <IgnatevaDE@osk.org.rzd>
2024-04-13 20:20:47 +0500

    GetCreateForumVC приведение к единому шаблону

--- SupportForum/wwwroot/js/site.js
+++ SupportForum/wwwroot/js/site.js
@@ -2,23 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function GetCreateForumVC(IdInitiator, IdCategory, IdParent = null) {
-    $.ajax({
-        type: "GET",
-        url: "/Forum/GetCreateForumVC",
-        data: {
-            IdInitiator, IdCategory, IdParent
-        }
 
-    }).done(function (data, statusText, xhdr) {
-        console.log("Done");
-        $("#pnlOffcanvasBody").html(data);
-        if (IdParent != null) $("#offcanvasActionForumLabel").text("Создать вложенный форум");
-        else $("#offcanvasActionForumLabel").text("Создать форум");
-    }).fail(function (xhdr, statusText, errorText) {
-        console.log("Failed");
-        $("#pnlOffcanvasBody").text(JSON.stringify(xhdr));
-    });
+function GetCreateForumVC(IdInitiator, IdCategory, IdParent = null) {
+    GetActionForumVC("/Forum/GetCreateForumVC",
+        {
+            idInitiator: IdInitiator,
+            idCategory: IdCategory,
+            idParent: IdParent
+        },
+        IdParent == null ? "Создать вложенный форум" : "Создать форум");
 }
 
 function GetCreateTopicVC(IdInitiator, IdForum, IdForumCat) {
