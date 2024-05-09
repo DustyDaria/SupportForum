6bcd5221ebe1529ff0498f493f2248013a7ba5d9
Игнатьева Дарья <IgnatevaDE@osk.org.rzd>
2024-05-09 16:42:52 +0500

    add Error.cs

---
+++ SupportForum/Helper/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace SupportForum.Helper
+{
+    public class Error
+    {
+        public static readonly string IncorrectInitiator = "Некорректная передача инициатора. ";
+        
+
+    }
+}
