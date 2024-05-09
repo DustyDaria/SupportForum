5cbaeefd68ceffa47cdb52d68d693887165bf3cf
Игнатьева Дарья <IgnatevaDE@osk.org.rzd>
2024-05-09 14:39:45 +0500

    upd TopicViewModel хранение данных древовидной структуры отдельным свойством

--- SupportForum/Models/ViewModels/TopicViewModel.cs
+++ SupportForum/Models/ViewModels/TopicViewModel.cs
@@ -3,6 +3,8 @@
     public class TopicViewModel
     {
         public TblTopic Topic { get; set; }
-        public decimal IdForumCategory { get; set; }
+        public decimal? IdForumCategory { get; set; }
+
+        public List<TblCommunication> communicationVM { get; set; } = new List<TblCommunication>();  
     }
 }
